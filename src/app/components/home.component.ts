1cd162a782e9d9b28f9c221ddba4ac1f9085c7dd
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-08 10:37:54 +0200

    Setup basic home page

---
+++ src/app/components/home.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle,} from '@angular/material/card';
+import {MatCalendar} from '@angular/material/datepicker';
+import {MatToolbar} from '@angular/material/toolbar';
+import {CommonModule, NgForOf, NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `<p>Home</p>
+  `,
+  imports: [
+    CommonModule,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCalendar,
+    MatCardContent,
+    MatToolbar,
+    NgIf,
+    RouterLink,
+    NgForOf,
+    MatProgressSpinner,
+  ],
+})
+export class HomeComponent {
+
+  constructor() {
+  }
+}
