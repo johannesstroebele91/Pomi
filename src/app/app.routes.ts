9cdba5131661a0c8dd3c1ff3c695bf5e368e4a6a
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-08 10:39:05 +0200

    Setup routing with authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AuthGuard} from "./services/auth-guard.service";
+import {LoginComponent} from "./components/login.component";
+import {HomeComponent} from "./components/home.component";
+import {RegistrationComponent} from "./components/registration.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LoginComponent, pathMatch: 'full'},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: '**', redirectTo: '/home'},
+
+];
